03c3e8d5eca25baeb33a383adf54a4a2ad32e0ae
xuxiao07 <xuxiao07@corp.netease.com>
2023-10-12 15:20:45 +0800

    update

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 import json
-
+import sys
 from flask import Flask, request
+from flask_cors import CORS
 import requests
 import socket
 import netifaces
 app = Flask(__name__)
+
+
+def after_request(resp):
+    resp.headers['Access-Control-Allow-Credentials'] = 'true'
+    return resp
+
+
+app.after_request(after_request)
+CORS(app)
 
 
 def returner(code, msg, data):
@@ -87,7 +97,60 @@ def access():
     return returner(200, "ok", response.text)
 
 
+@app.route('/get_version', methods=["GET"])
+def get_version():
+    # 获取本地ip
+    color_mark = request.headers.get('X-Nsf-Mark')
+    return returner(200, "ok", {
+        "version": "V1.0.0",
+        "notes": "提供产品的详细介绍信息，为product-info提供年化利率的数据（非核心）",
+        "ips": get_local_ip(),
+        "headers": str(request.headers),
+        "color_mark": color_mark
+    })
+
+
+@app.route('/get_product_detail', methods=["GET"])
+def get_product_detail():
+    # 获取本地ip
+
+    return returner(200, "ok", [
+        {
+            "title": "财富管家-初级版",
+            "detail": "财富管家-中级版主要投向中高等级<span style='color: #1976D2'>中短期债券</span>，远离股市波动。所投债券资产久期较短且信用等级较高，相对能更好地控制风险，力争匹配投资者的<span style='color: #1976D2'>短期闲钱理财需求</span>。"     # "<span style='color: #FE0000'>这是一个bug，本来这里要显示文字的！</span>"
+        },
+        {
+            "title": "财富管家-中级版",
+            "detail": "财富管家-中级版主要投向中高等级<span style='color: #1976D2'>中短期债券</span>，远离股市波动。所投债券资产久期较短且信用等级较高，相对能更好地控制风险，力争匹配投资者的<span style='color: #1976D2'>短期闲钱理财需求</span>。"
+        },
+        {
+            "title": "财富管家-高级版",
+            "detail": "财富管家-高级版主要投向中高等级<span style='color: #1976D2'>中短期债券</span>，远离股市波动。所投债券资产久期较短且信用等级较高，相对能更好地控制风险，力争匹配投资者的<span style='color: #1976D2'>短期闲钱理财需求</span>。"
+        },
+        {
+            "title": "财富管家-终身版",
+            "detail": "财富管家-终身版主要投向中高等级<span style='color: #1976D2'>中短期债券</span>，远离股市波动。所投债券资产久期较短且信用等级较高，相对能更好地控制风险，力争匹配投资者的<span style='color: #1976D2'>短期闲钱理财需求</span>。"
+        },
+    ])
+
+
+@app.route('/get_annual', methods=["GET"])
+def get_annual():
+    product_name = request.args.get("product_name")
+    annual = "0.00%"
+    if product_name == "财富管家-初级版":
+        annual = "3.33%"
+    if product_name == "财富管家-中级版":
+        annual = "3.60%"
+    if product_name == "财富管家-高级版":
+        annual = "4.49%"
+    if product_name == "财富管家-终身版":
+        annual = "5.06%"
+    return annual
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    port = int(sys.argv[1]) if len(sys.argv) > 1 else 8081
+    app.run(host='0.0.0.0', port=port)
 
 
